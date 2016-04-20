7ea0d3e5a02fd4584f8a129f5bb8d017ea969ea3
Hackbright Student <info@hackbrightacademy.com>
2016-04-19 17:56:40 -0700

    further study finish 1

--- madlibs.py
+++ madlibs.py
@@ -59,16 +59,38 @@ def show_madlib():
     color_word = request.args.get("color")
     noun_word = request.args.get("noun")
     adjective_word = request.args.get("adjective")
+    place_word = request.args.get("place")
+    famous_people_str = ""
+    famous_people = []
+    if request.args.get("shakira"):
+        shakira = request.args.get("shakira")
+        famous_people.append(shakira)
+    if request.args.get("madonna"):
+        madonna = request.args.get("madonna")
+        famous_people.append(madonna)
+    if request.args.get("beyonce"):
+        beyonce = request.args.get("beyonce")
+        famous_people.append(beyonce)
+    else:
+        famous_people_str = "No one"
+
+    if famous_people != "No one":
+        for person in famous_people:
+            famous_people_str = famous_people_str + ", " + person
+            famous_people_str = famous_people_str.lstrip(", ")
 
 
     return render_template("madlib.html",
                             person=player,
                             color=color_word,
                             noun=noun_word,
-                            adjective=adjective_word)
+                            adjective=adjective_word,
+                            place=place_word,
+                            people=famous_people_str)
 
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
     # if we change the code.
     app.run(debug=True)
+
